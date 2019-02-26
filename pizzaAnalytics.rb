783c87c8107176ea0c24b10b7b2276eb2f0b3172
TRAVIS RisTow <TRtheGreat19@gmail.com>
2019-02-26 14:14:28 -0800

    Learning how to pull CSV into PostgreSQL

--- pizzaAnalytics.rb
+++ pizzaAnalytics.rb
@@ -1,1 +1,44 @@
-print "Travis is the best" 
+require 'pg'
+require 'csv'
+
+CSV.foreach("data.csv") do |row|
+  puts row.inspect
+end
+print "\n\n"
+
+
+conn = PG.connect(
+  dbname: "postgres",
+  port: 4720,
+  user: "postgres",
+  password: "admin"
+)
+
+
+conn.exec "DROP TABLE IF EXISTS People"
+conn.exec "CREATE TABLE People(
+			Id INTEGER PRIMARY KEY, 
+	        Name VARCHAR(20)
+        )"
+
+
+i = 0
+CSV.foreach("data.csv") do |row|
+	conn.exec_params(
+    "INSERT INTO People (id, name)
+    values ($1, $2);",
+    [
+      i,
+      row[0]
+    ])
+	i+=1
+end
+
+
+# conn.exec "INSERT INTO People VALUES(1, 'Steve')"
+
+
+result = conn.exec "SELECT * FROM People"
+result.each do |row|
+	puts row.inspect
+end
